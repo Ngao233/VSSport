9addfba832850b2fbeb6301290ef022156b6d817
caotuanvy <vyctps41133@gmail.com>
2024-11-08 07:58:34 +0700

    Phần in áo

---
+++ javascrip.js
@@ -0,0 +1,14 @@
+var textN = document.getElementById('textN');
+        var numberN = document.getElementById('numberN');
+        function Textaction() {
+            textN.innerHTML = "";
+            var Name = document.getElementById('nametext').value;
+            textN.innerHTML = Name;
+        }
+        function Numberr() {
+            numberN.innerHTML = "";
+            var Numberr = document.getElementById('numbertext').value;
+            numberN.innerHTML = Numberr;
+        }
+
+
