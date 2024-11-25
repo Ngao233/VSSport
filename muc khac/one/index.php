dc379bc4cd53831a9efc925181254a80dff0d621
minh <minhnhhps41222@gmail.com>
2024-11-25 08:17:36 +0700

    MINH

--- muc khac/one/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-ob_start();
-session_start();
-include 'controllers/index.php';
--- muc khac/one/model/cart.php
+++
@@ -1,9 +0,0 @@
-<?php 
-class cart extends connect{
-    function add_cart($id_product){
-        $conn = new connect();
-        $sql = "INSERT INTO cart(id_product,id_user) values(?,?)";
-        $params = [$id_product,$_SESSION['user']['id_user']];
-        $result = $this -> query_user($sql,$params);
-    }
-}
