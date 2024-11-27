16fb9383cf3f0a03f10f34e71c7e1437e03f7a0d
Namhqps41211 <namhqps41211@gmail.com>
2024-11-27 16:36:20 +0700

    products-web

---
+++ Test 1/data.js
@@ -0,0 +1,7 @@
+sanpham  = {
+    id_sp:"SP01", tensp:"Quần jean", hinhanh:["hinh1.jpg","hinh2.jpg"]
+}
+hinhanh = [
+    {id_hinh:1, tenhinh:"hinh1.jpg", id_sp:"SP01"},
+    {id_hinh:2, tenhinh:"hinh2.jpg", id_sp:"SP02"}
+];
