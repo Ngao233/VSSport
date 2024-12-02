ff47e2a2b11af58777c58c952040c4c57060f458
caotuanvy <vyctps41133@gmail.com>
2024-12-02 07:13:12 +0700

    cá

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -5,7 +5,6 @@ include_once "model/register.php";
 
 switch ($action) {  
     case '':  
-        
         $product = getProduct();  
         include "views/home.php";  
         break;   
@@ -34,4 +33,13 @@ switch ($action) {
             exit; // Thêm exit sau header để dừng thực hiện mã tiếp theo  
         }    
         break;  
+        case 'dangnhap':
+            include "model/login.php";
+            include "views/dangnhap.php";  
+            break;
+        case 'profile':
+            include "views/profile.php";
+            break;
+
+
 }
