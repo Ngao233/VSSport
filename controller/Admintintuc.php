a9ceb702cebeb1c74d6d45ed955a4fbebbd81001
duongunderoud <nqchi123@gmail.com>
2024-12-02 22:49:08 +0700

    aaaaaaaaaaaaaaaaaaaaaa

--- controller/Admintintuc.php
+++ controller/Admintintuc.php
@@ -10,34 +10,34 @@ switch ($action) {
         include "admin/tintuc/HomeTinTuc.php";
         include "admin/FooterAdmin.php";
         break;
-    // case 'edittintuc':
-    //         $id = $_GET["id"] ?? "";  
-    //         $tintuc = getTinTucid($id);
-    //         include "admin/tintuc/edit.php";
-    //         break;
+    case 'edittintuc':
+            $id = $_GET["id"] ?? "";  
+            $tintuc = getTinTucid($id);
+            include "admin/tintuc/edit.php";
+            break;
         
-    //         case "updatetintuc":
-    //             $id = $_GET["id"] ?? "";
-    //             $tintuc = getTinTucid($id); 
+            case "updatetintuc":
+                $id = $_GET["id"] ?? "";
+                $tintuc = getTinTucid($id); 
 
-    //             $TieuDe=trim($_POST["TieuDe"])??"";
+                $TieuDe=trim($_POST["TieuDe"])??"";
                 
-    //             $NgayDang=trim($_POST["NgayDang"]) ?? "";
+                $NgayDang=trim($_POST["NgayDang"]) ?? "";
 
-    //             $HinhAnh = trim($_POST["HinhAnh"] ?? "");
+                $HinhAnh = trim($_POST["HinhAnh"] ?? "");
 
-    //             $NoiDung = trim($_POST["Noidung"] ?? "");  
+                $NoiDung = trim($_POST["Noidung"] ?? "");  
 
-    //             $id=$_GET["id"]??"";
-    //             $tintuc = getTinTucid($id);
-    //             include "admin/tintuc/edit.php";
-    //             updateTinTuc($id,$TieuDe,$NgayDang,$HinhAnh,$NoiDung);
-    //                 header("Location: $base_url/tintucAdmin");
+                $id=$_GET["id"]??"";
+                $tintuc = getTinTucid($id);
+                include "admin/tintuc/edit.php";
+                updateTinTuc($id,$TieuDe,$NgayDang,$HinhAnh,$NoiDung);
+                    header("Location: $base_url/tintucAdmin");
      
-    //             break; 
-    //             case "deleteoder":
-    //                 $id=$_GET["id"]??"";
-    //                 deleteTinTuc($id);
-    //                 header("Location: $base_url/oderAdmin");
-    //                 break;
+                break; 
+                case "deleteoder":
+                    $id=$_GET["id"]??"";
+                    deleteTinTuc($id);
+                    header("Location: $base_url/oderAdmin");
+                    break;
                 }
