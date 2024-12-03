31cc11dc5c01bcf9a63a244ff8c50676ee716c09
duongunderoud <nqchi123@gmail.com>
2024-12-03 11:02:42 +0700

    datten

--- controller/Admintintuc.php
+++ controller/Admintintuc.php
@@ -26,18 +26,34 @@ switch ($action) {
 
                 $HinhAnh = trim($_POST["HinhAnh"] ?? "");
 
-                $NoiDung = trim($_POST["Noidung"] ?? "");  
+                $NoiDung = trim($_POST["NoiDung"] ?? "");  
 
-                $id=$_GET["id"]??"";
+                $id=$_GET["id"]??"";    
                 $tintuc = getTinTucid($id);
                 include "admin/tintuc/edit.php";
                 updateTinTuc($id,$TieuDe,$NgayDang,$HinhAnh,$NoiDung);
                     header("Location: $base_url/tintucAdmin");
      
                 break; 
-                case "deleteoder":
+                case "deletetintuc":
                     $id=$_GET["id"]??"";
                     deleteTinTuc($id);
                     header("Location: $base_url/oderAdmin");
                     break;
+                
+                
+                case "posttintuc":
+                $tintuc = getTinTucid($id); 
+
+                $TieuDe=trim($_POST["TieuDe"])??"";
+                
+                $NgayDang=trim($_POST["NgayDang"]) ?? "";
+
+                $HinhAnh = trim($_POST["HinhAnh"] ?? "");
+
+                $NoiDung = trim($_POST["NoiDung"] ?? "");
+                    include "admin/tintuc/add.php";
+                        addTinTuc($id,$TieuDe,$NgayDang,$HinhAnh,$NoiDung);
+                        header("Location: $base_url");    
+                    break;   
                 }
