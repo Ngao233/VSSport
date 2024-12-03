791e67b80588fe4dabe542e83922e38d750d4c65
namhqps41211 <namhqps41211@gmail.com>
2024-12-04 01:51:21 +0700

    bình luận và tin tức

--- controller/T.home.php
+++ controller/T.home.php
@@ -9,20 +9,15 @@ switch ($action) {
         include_once "views/header.php"; 
         include "views/home.php";  
         include_once "views/footer.php";
-<<<<<<< HEAD
+    break;
+    case 'statistics':
+        include "admin/statistics.php";
         break;
-        case 'statistics':
-            include "admin/statistics.php";
-            break;
-        case 'home':  
-            $product = getProduct(); 
-            include_once "views/header.php"; 
-            include "views/home.php";  
-            include_once "views/footer.php";
-            break;
-               
-=======
-        break;  
->>>>>>> 83cddcf9fc95eb14b763bed4cc3f09dfec9561a5
-        }
+    case 'home':  
+        $product = getProduct(); 
+        include_once "views/header.php"; 
+        include "views/home.php";  
+        include_once "views/footer.php";
+        break;
+}
         
