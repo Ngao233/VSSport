bcffe816e9fd3a0e6b1547f7d6c1f959bf94a41b
caotuanvy <vyctps41133@gmail.com>
2024-12-07 15:04:32 +0700

    ...

--- controller/T.chitietsp.php
+++ controller/T.chitietsp.php
@@ -1,11 +1,14 @@
 <?php
 
 include_once "model/chitietsp.php";
+include_once "model/product.php";  
 switch ($action) {   
     case 'chitietsp': 
         $id = $_GET["id"] ?? "";  
+        $productdetail = getProductDetail($id);
+        $product1 = getProductWithDiscount();
         $product = getProductid($id);
         include "views/chitietsp.php";  
         break;  
         
-}
+}   
