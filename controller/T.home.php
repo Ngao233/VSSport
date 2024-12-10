60c7f408edefdb811b9147e4cc9dddd2230f3db2
namhqps41211 <namhqps41211@gmail.com>
2024-12-10 20:14:45 +0700

    static

--- controller/T.home.php
+++ controller/T.home.php
@@ -13,9 +13,7 @@ switch ($action) {
         include "views/home.php";  
         include_once "views/footer.php";
     break;
-    case 'statistics':
-        include "admin/statistics.php";
-        break;
+
     case 'home':  
         $product1 = getProductWithDiscount();
         $product = getProduct();
